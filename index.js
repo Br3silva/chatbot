8dffa46726020e0327c17fe90566430ca12fc990
br3silva <119340372+Br3silva@users.noreply.github.com>
2022-12-31 15:21:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+	
+    function test(){
+        
+        var a = document.getElementById ("teste3").value;
+        var b = document.getElementById ("teste4").value;
+
+
+        var soma = parseInt(a)+ parseInt(b);
+        alert (soma);
+       
+       
+        
+    }
