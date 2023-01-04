73ae029c4a09a0dc636af4ce8f04aeade23ae436
br3silva <119340372+Br3silva@users.noreply.github.com>
2023-01-04 17:07:01 -0300

    update

--- connBD.php
+++ connBD.php
@@ -1,7 +1,20 @@
 <?php
 
+$host ="localhost";
+$nameserver= "root";
+$password= "";
+$database = "bd_relatorio";
+
+$conn= mysqli_connect("$host","$nameserver","$password","$database");
 
 
+if (!$conn){
+
+die ("conexao mal sucedida" . mysqli_connect_error());
+
+}
+
+echo  "conexao sucedida";
 
 
 ?>
